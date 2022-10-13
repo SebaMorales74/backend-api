d3fe608776150adb281efa0dc48134686bd56cef
CholGH <smtorresmotante@gmail.com>
2022-10-12 22:36:34 -0300

    Init

---
+++ src/features/Producto/index.jsx
@@ -0,0 +1,17 @@
+import { Card } from 'antd';
+import React from 'react';
+
+const Producto = () => (
+    <>
+        <Card
+            title="Default size card"
+            style={{ width: 300 }}
+        >
+            <p>Card content</p>
+            <p>Card content</p>
+            <p>Card content</p>
+        </Card>
+    </>
+);
+
+export default Producto;
